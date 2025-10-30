f9eedf74219e5bb918ef91d519e02a192e32e0e3
Paco Recio <jfrecio@gmail.com>
2025-10-30 16:26:28 +0100

    Ultima subida del curso

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,11 +1,22 @@
 import groovy.io.FileType
 import com.cloudbees.groovy.cps.NonCPS
 
+import java.text.SimpleDateFormat
+
+//Al ponerlo como def call es necesario que este script este en vars.
+//Tambien es necesario definir en Jenkins, en Administrar Jenkins -> System la parte de "Global Trusted Pipeline Libraries"
 def call(Map config = [:]) {
     def ws = pwd()
     def content = buildReleaseNotes(ws)       // @NonCPS: recorre el FS y devuelve String
     writeFile file: "${ws}/releasenotes.txt", text: content
     echo "Release notes generadas en ${ws}/releasenotes.txt"
+
+    //Fecha y hora actuales
+    def date = new Date()
+    def sdf = new SimpleDateFormat('MM/dd/yyyy HH:mm:ss')
+    echo "Fecha y hora actuales: " + sdf.format(date)
+
+
 }
 
 @NonCPS
